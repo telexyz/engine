2b14a3f50067f82d8664ce79a69b9e51913e3c35
Alex Nguyen <tiendung@users.noreply.github.com>
2021-06-25 22:53:47 +0700

    skip

--- check.sh
+++ check.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-zig test src/syllable_data.zig
+# zig test src/syllable_data.zig
 
-zig test src/parsers.zig
+# zig test src/parsers.zig
 
-zig test src/converters.zig
+# zig test src/converters.zig
 
 zig test test_against_vn_syllables.zig
 
