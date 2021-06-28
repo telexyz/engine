f73c811be7a055db5756013bd18515e465fe0571
Alex Nguyen <tiendung@users.noreply.github.com>
2021-06-28 16:02:12 +0700

    cleanup

--- src/text_utils.zig
+++ src/text_utils.zig
@@ -12,65 +12,70 @@ fn printNothing(comptime fmt_str: []const u8, args: anytype) void {
 }
 
 const PAD = "                 ";
+const WAIT_NANOSECS: u64 = 600_000_000; // nanoseconds
+
 pub fn telexifyAlphabetTokens(text: *Text) void {
     @setRuntimeSafety(false);
+    var char_stream = U2ACharStream.new();
     var prev_percent: u64 = 0;
-
     const max_sleeps: u8 = 1;
-    const sleep_time: u64 = 600_000_000; // nanosec
     var sleeps_count: u8 = 0;
 
     var i: *usize = &text.processed_types_count;
     while (i.* <= text.tokens_number) : (i.* += 1) {
         // Check if reach the end of tokens list
         if (i.* == text.tokens_number) {
-            // If no more tokens for sure then return
+            // If segmentation end => no more tokens for sure then return
             if (text.tokens_number_finalized) return;
             // BEGIN waiting for new tokens (all tokens is processed)
             while (sleeps_count < max_sleeps and i.* == text.tokens_number) {
-                std.time.sleep(sleep_time);
+                std.time.sleep(WAIT_NANOSECS);
                 sleeps_count += 1;
                 std.debug.print("{s}... wait new tokens\n", .{PAD});
-            }
-            if (i.* == text.tokens_number) {
-                // No new token and timeout
-                return;
-            } else {
-                // Reset sleep counter and continue
-                sleeps_count = 0;
-            }
-        } // END waiting for new tokens
+            } // END waiting for new tokens
+            // No new token and timeout
+            if (i.* == text.tokens_number) return;
+            // Got new tokens, reset counter and continue
+            sleeps_count = 0;
+        }
 
-        // Init token
+        // Init token shortcuts
         var token = text.tokens[i.*];
-
-        // is NewLine token
         if (token[0] == '\n') {
             recordNewLineTokenAndShowProgress(text, i.*, &prev_percent);
             continue;
         }
-
-        //  and it's attributes shortcuts
+        //  and token's attributes shortcut
         var attrs = &text.tokens_attrs[i.*];
-        var token_written = false;
+        var token_not_written = true;
 
         // Reserver first-byte to write token attrs
         const firt_byte_index = text.transformed_bytes_len;
         text.transformed_bytes_len += 1;
 
         if (attrs.category == .alphabet or attrs.category == .marktone) {
-            // Since token is alphabet, it's alphabet_types[i]'s info must existed
+            // marktone is alphabet and both count to alphabet_types
+            // Since token is alphabet, it's alphabet_types' info must existed
             const type_info = text.alphabet_types.getPtr(token).?;
-            if (type_info.category == ._none) {
-                if (syllablizeToken(text, token, type_info)) {
-                    // Mark token_written = true to know this is the first token
-                    // point to this syllable trans (don't need to write twice)
-                    token_written = true;
+            if (type_info.category == ._none) { // Not transformed yet
+                char_stream.reset();
+                // Try to convert token to syllable
+                const syllable = parsers.parseTokenToGetSyllable(true, printNothing, &char_stream, token);
+                if (syllable.can_be_vietnamese) {
+                    // Token is vietnamese syllabe
+                    type_info.category = .syllable;
+                    // Write ascii-telex transform
+                    const syllable_token = saveAsciiTelexTransform(text, char_stream);
+                    type_info.transform = syllable_token;
+                    countSyllableAndSyllower(text, syllable_token, type_info.count) catch unreachable;
                 } else {
-                    // For non-syllable, attrs.category can only be .alphabet or .marktone
+                    token_not_written = false;
+                    // For non-syllable, attrs.category can only be
+                    // .alphabet or .marktone
                     type_info.*.category = attrs.category;
                 }
             }
+
             if (type_info.*.category == .syllable) {
                 attrs.category = .syllable;
                 // Point token value to it's syllable trans
@@ -79,7 +84,7 @@ pub fn telexifyAlphabetTokens(text: *Text) void {
             }
         } // attrs.category == .alphabet or .marktone
 
-        if (!token_written) {
+        if (token_not_written) {
             // write original token it's is not syllable
             for (token) |b| {
                 text.transformed_bytes[text.transformed_bytes_len] = b;
@@ -88,30 +93,9 @@ pub fn telexifyAlphabetTokens(text: *Text) void {
         }
         // Write attrs at the begin of token's ouput stream
         text.transformed_bytes[firt_byte_index] = @bitCast(u8, attrs.*);
-    }
+    } // END while text.processed_types_count
 }
 
-fn syllablizeToken(text: *Text, token: []const u8, type_info: *Text.TypeInfo) bool {
-    // Not transformed yet
-    var char_stream = U2ACharStream.new();
-    const syllable = parsers.parseTokenToGetSyllable(
-        true,
-        printNothing,
-        &char_stream,
-        token,
-    );
-
-    if (syllable.can_be_vietnamese) {
-        // First time convert type to syllable
-        type_info.category = .syllable;
-        // Point token value to it's syllable trans
-        const syllable_token = saveAsciiTelexTransform(text, char_stream);
-        type_info.transform = syllable_token;
-        countSyllableAndSyllower(text, syllable_token, type_info.count) catch unreachable;
-        return true;
-    }
-    return false;
-}
 fn recordNewLineTokenAndShowProgress(text: *Text, token_index: usize, prev_percent: *u64) void {
     text.transformed_bytes[text.transformed_bytes_len] = '\n';
     text.transformed_bytes_len += 1;
