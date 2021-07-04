ef7d4b38fa95e42cc312ba406a49317580435f8a
Alex Nguyen <tiendung@users.noreply.github.com>
2021-07-04 16:51:57 +0700

    update

--- play_with_chars.zig
+++ play_with_chars.zig
@@ -7,7 +7,7 @@ const unicode = std.unicode;
 // Dùng được 27 invisible ascii chars, 1-8, 11,12, 15-31
 // var str = "|\x01\x02\x03\x04\x05\x06\x07\x08\x0b\x0c\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f|";
 
-var str = " ";
+var str = " ̣ ̂́ ̉ ̂ ̛̣";
 
 //\n\tđĐàáãâăèéêìíĩòóõôơýùúũưủụọỏịỉỳỵỷỹạảẹẻẽẦẤẬẨẪẰẮẶẲẴỀẾỆỂỄỒỐỘỔỖỜỚỢỞỠỪỨỰỬỮầấậẩẫằắặẳẵềếệểễồốộổỗờớợởỡừứựửữ";
 
@@ -42,7 +42,8 @@ pub fn main() !void {
         const char_bytes_length = try unicode.utf8ByteSequenceLength(byte);
         next_index = next_index + char_bytes_length;
         // if (byte & 0b1100010 != 0b1100010) continue;
-        print("'{s}'", .{str[index..next_index]});
+        const char = try unicode.utf8Decode(str[index..next_index]);
+        print("'{s}' => {d}", .{ str[index..next_index], char });
         var i: usize = 0;
         while (i < char_bytes_length) {
             const b = str[index + i];
