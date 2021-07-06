80aa934d0092f393b18a290fafad9beb7624f754
Alex Nguyen <tiendung@users.noreply.github.com>
2021-07-06 19:14:27 +0700

    refine

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@ rm data/1*.txt
 
 ~/zig build && ./zig-out/bin/telexify data/news_titles.txt data/news_titles_tknz.txt
 
-# ~/zig run telexify.zig -O ReleaseFast -- ../corpus/fb_comments_10m.txt ../corpus/fb_comments_10m_tknz.txt
+# ~/zig run telexify.zig -O ReleaseFast -- ../data/fb_comments_10m.txt ../data/fb_comments_10m_tknz.txt
 
-# ~/zig run telexify.zig -O ReleaseFast -- ../corpus/best_vi_translation_train.txt ../corpus/best_vi_translation_train_tknz.txt
+# ~/zig run telexify.zig -O ReleaseFast -- ../data/best_vi_translation_train.txt ../data/best_vi_translation_train_tknz.txt
 
-# ~/zig run telexify.zig -O ReleaseFast -- ../corpus/news_titles_10m.txt ../corpus/news_titles_10m_tknz.txt
+# ~/zig run telexify.zig -O ReleaseFast -- ../data/news_titles_10m.txt ../data/news_titles_10m_tknz.txt
 
-# ~/zig run telexify.zig -- ../corpus/VNESEcorpus.txt ../corpus/VNESEcorpus_tknz.txt
+# ~/zig run telexify.zig -- ../data/VNESEcorpus.txt ../data/VNESEcorpus_tknz.txt
 
-# ~/zig run telexify.zig -O ReleaseSafe -- ../corpus/VNTQcorpus.txt ../corpus/VNTQcorpus_tknz.txt
+# ~/zig run telexify.zig -O ReleaseSafe -- ../data/VNTQcorpus.txt ../data/VNTQcorpus_tknz.txt
