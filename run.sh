202e4ed2b2d3532b7dbfffa5a0092d1a6fef02fb
Alex Nguyen <tiendung@users.noreply.github.com>
2021-07-06 19:12:43 +0700

    refine

--- run.sh
+++ run.sh
@@ -9,10 +9,8 @@ rm data/1*.txt
 
 # ~/zig run telexify.zig -O ReleaseFast -- ../corpus/best_vi_translation_train.txt ../corpus/best_vi_translation_train_tknz.txt
 
-# ~/zig run telexify.zig -O ReleaseFast -- ../corpus/corpus-title.txt ../corpus/corpus-title_tknz.txt
+# ~/zig run telexify.zig -O ReleaseFast -- ../corpus/news_titles_10m.txt ../corpus/news_titles_10m_tknz.txt
 
 # ~/zig run telexify.zig -- ../corpus/VNESEcorpus.txt ../corpus/VNESEcorpus_tknz.txt
 
 # ~/zig run telexify.zig -O ReleaseSafe -- ../corpus/VNTQcorpus.txt ../corpus/VNTQcorpus_tknz.txt
-
-# ~/zig run telexify.zig -- ../corpus/corpus-title-sample.txt ../corpus/corpus-title-sample_tknz.txt 1000
