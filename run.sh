9c80eb878b0187556d041ea7c4adce333a71d50d
Giang <giang@MacBook-Air-cua-Giang.local>
2021-07-07 14:49:26 +0700

    refine

--- run.sh
+++ run.sh
@@ -4,15 +4,4 @@ rm data/0*.txt
 rm data/1*.txt
 
 ~/zig build -Drelease-fast=true
-# ./zig-out/bin/telexify data/news_titles.txt data/news_titles_tknz.txt
-
-./zig-out/bin/telexify ../data/fb_comments.txt ../data/fb_comments_tknz.txt
-
-# ./zig-out/bin/telexify ../data/vietai_sat.txt ../data/vietai_sat_tknz.txt
-
-# ./zig-out/bin/telexify ../data/news_titles.txt ../data/news_titles_tknz.txt
-
-# ~/zig run telexify.zig -- ../data/VNESEcorpus.txt ../data/VNESEcorpus_tknz.txt
-
-# ~/zig run telexify.zig -O ReleaseSafe -- ../data/VNTQcorpus.txt ../data/VNTQcorpus_tknz.txt
-
+./zig-out/bin/telexify data/news_titles.txt data/news_titles_tknz.txt ON
