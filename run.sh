e90aa57e7ace0fb0825a201e1f50843d94765822
Alex Nguyen <tiendung@users.noreply.github.com>
2021-07-12 08:56:02 +0700

    update

--- run.sh
+++ run.sh
@@ -4,6 +4,5 @@ rm data/0*.txt
 rm data/1*.txt
 
 ~/zig build -Drelease-fast=true
-# ./zig-out/bin/telexify data/news_titles.txt data/news_titles_tknz.txt
-./zig-out/bin/telexify ../corpus/vietai_sat.txt data/vietai_sat_tknz.txt
-
+# ./zig-out/bin/telexify data/news_titles.txt data/tknz.txt
+./zig-out/bin/telexify ../corpus/vietai_sat.txt data/tknz.txt
