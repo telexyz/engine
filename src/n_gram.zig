90c45eedbf0394fa52b7a73fffc8aacea5dac184
Tien Dung <dungtn@gmail.com>
2021-07-21 15:20:36 +0700

    add four_gram

--- src/n_gram.zig
+++ src/n_gram.zig
@@ -7,60 +7,79 @@ const syllable_data_structs = @import("./syllable_data_structs.zig");
 const Syllable = syllable_data_structs.Syllable;
 const Text = @import("./text_data_struct.zig").Text;
 
-const BiTriGram = packed struct {
+const Gram = packed struct {
     const BLANK: Syllable.UniqueId = 0;
 
     s0: Syllable.UniqueId = BLANK,
     s1: Syllable.UniqueId = BLANK,
     s2: Syllable.UniqueId = BLANK,
+    s3: Syllable.UniqueId = BLANK,
 
-    fn isTriSyllables(self: BiTriGram) bool {
-        return self.s0 != BLANK and self.s1 != BLANK and self.s2 != BLANK;
+    fn isFourGram(self: Gram) bool {
+        return self.s0 != BLANK and self.s1 != BLANK and
+            self.s2 != BLANK and self.s3 != BLANK;
     }
 
-    fn isBiSyllables(self: BiTriGram) bool {
-        return self.s1 != BLANK and self.s2 != BLANK;
+    fn isTriGram(self: Gram) bool {
+        return self.s1 != BLANK and
+            self.s2 != BLANK and self.s3 != BLANK;
     }
 
-    pub fn printToBuffUtf8(self: BiTriGram, buffer: []u8) []const u8 {
+    fn isBiGram(self: Gram) bool {
+        return true and
+            self.s2 != BLANK and self.s3 != BLANK;
+    }
+
+    pub fn printToBuffUtf8(self: Gram, buffer: []u8) []const u8 {
         var n: usize = 0;
-        var buff: []u8 = buffer;
+        var buff: []u8 = undefined;
 
         if (self.s0 != BLANK) {
+            buff = buffer[n..];
             n += Syllable.newFromId(self.s0).printBuffUtf8(buff).len;
             buffer[n] = 32;
             n += 1;
         }
 
+        if (self.s1 != BLANK) {
+            buff = buffer[n..];
+            n += Syllable.newFromId(self.s1).printBuffUtf8(buff).len;
+            buffer[n] = 32;
+            n += 1;
+        }
+
         buff = buffer[n..];
-        n += Syllable.newFromId(self.s1).printBuffUtf8(buff).len;
+        n += Syllable.newFromId(self.s2).printBuffUtf8(buff).len;
 
         buffer[n] = 32;
         n += 1;
 
         buff = buffer[n..];
-        n += Syllable.newFromId(self.s2).printBuffUtf8(buff).len;
+        n += Syllable.newFromId(self.s3).printBuffUtf8(buff).len;
 
         return buffer[0..n];
     }
 };
 
-const BiTriGramCount = std.AutoHashMap(BiTriGram, u32);
+const GramCount = std.AutoHashMap(Gram, u32);
 
 pub const NGram = struct {
-    bi_gram_counts: BiTriGramCount = undefined,
-    tri_gram_counts: BiTriGramCount = undefined,
+    bi_gram_counts: GramCount = undefined,
+    tri_gram_counts: GramCount = undefined,
+    four_gram_counts: GramCount = undefined,
 
     pub const MIN_COUNT = 0;
 
     pub fn init(self: *NGram, allocator: *std.mem.Allocator) void {
-        self.bi_gram_counts = BiTriGramCount.init(allocator);
-        self.tri_gram_counts = BiTriGramCount.init(allocator);
+        self.bi_gram_counts = GramCount.init(allocator);
+        self.tri_gram_counts = GramCount.init(allocator);
+        self.four_gram_counts = GramCount.init(allocator);
     }
 
     pub fn deinit(self: *NGram) void {
         self.bi_gram_counts.deinit();
         self.tri_gram_counts.deinit();
+        self.four_gram_counts.deinit();
     }
 
     pub const Error = error{
@@ -70,10 +89,10 @@ pub const NGram = struct {
         if (!text.tokens_number_finalized) return Error.TextNotFinalized;
         _ = self;
 
-        // var buffer: [16 * 3]u8 = undefined;
+        // var buffer: [18 * 4]u8 = undefined;
         // const buff = buffer[0..];
 
-        var bi_tri_gram: BiTriGram = .{};
+        var gram: Gram = .{};
         var n = text.tokens_number;
         var i: usize = 0;
 
@@ -83,28 +102,37 @@ pub const NGram = struct {
 
             // if (is_syllable) std.debug.print("{s} ", .{text.tokens[i]}) else std.debug.print("\n", .{}); //OK
 
-            bi_tri_gram.s0 = bi_tri_gram.s1;
-            bi_tri_gram.s1 = bi_tri_gram.s2;
-            bi_tri_gram.s2 = if (is_syllable) text.syllable_ids[i] else BiTriGram.BLANK;
+            gram.s1 = gram.s2;
+            gram.s2 = gram.s3;
+            gram.s3 = if (is_syllable) text.syllable_ids[i] else Gram.BLANK;
 
-            if (bi_tri_gram.isTriSyllables()) {
-                // std.debug.print(" |{s}| ", .{bi_tri_gram.printToBuffUtf8(buff)}); //OK
-                const gop = try self.tri_gram_counts.getOrPutValue(bi_tri_gram, 0);
+            if (gram.isFourGram()) {
+                // std.debug.print(" |{s}| ", .{gram.printToBuffUtf8(buff)}); //OK
+                const gop = try self.four_gram_counts.getOrPutValue(gram, 0);
                 gop.value_ptr.* += 1;
             }
 
-            bi_tri_gram.s0 = BiTriGram.BLANK;
-            if (bi_tri_gram.isBiSyllables()) {
-                // std.debug.print(" |{s}| ", .{bi_tri_gram.printToBuffUtf8(buff)});//OK
-                const gop = try self.bi_gram_counts.getOrPutValue(bi_tri_gram, 0);
+            gram.s0 = Gram.BLANK;
+            if (gram.isTriGram()) {
+                // std.debug.print(" |{s}| ", .{gram.printToBuffUtf8(buff)}); //OK
+                const gop = try self.tri_gram_counts.getOrPutValue(gram, 0);
                 gop.value_ptr.* += 1;
             }
+
+            const temp = gram.s1;
+            gram.s1 = Gram.BLANK;
+            if (gram.isBiGram()) {
+                // std.debug.print(" |{s}| ", .{gram.printToBuffUtf8(buff)});//OK
+                const gop = try self.bi_gram_counts.getOrPutValue(gram, 0);
+                gop.value_ptr.* += 1;
+            }
+            gram.s0 = temp; // s0 := s1
         }
     }
 };
 
 const GramInfo = struct {
-    value: BiTriGram,
+    value: Gram,
     count: u32,
 };
 
@@ -113,8 +141,8 @@ fn order_by_count_desc(context: void, a: GramInfo, b: GramInfo) bool {
     return a.count > b.count;
 }
 
-pub fn writeGramCounts(grams: BiTriGramCount, filename: []const u8) !void {
-    var buffer: [16 * 3]u8 = undefined;
+pub fn writeGramCounts(grams: GramCount, filename: []const u8) !void {
+    var buffer: [18 * 4]u8 = undefined;
     const buff = buffer[0..];
 
     // Init list
@@ -139,8 +167,12 @@ pub fn writeGramCounts(grams: BiTriGramCount, filename: []const u8) !void {
     var wrt = std.io.bufferedWriter(file.writer());
 
     for (grams_list.items) |gram| {
-        try wrt.writer().print("{d} {s}\n", .{ gram.count, gram.value.printToBuffUtf8(buff) });
+        try wrt.writer().print("{d} {s}\n", .{
+            gram.count,
+            gram.value.printToBuffUtf8(buff),
+        });
     }
+
     try wrt.flush();
 }
 
