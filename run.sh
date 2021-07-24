2c9c36dba2cedb4efebd9a53baf767b3df8ef817
Tien Dung <dungtn@gmail.com>
2021-07-24 11:10:23 +0700

    try to build for windows

--- run.sh
+++ run.sh
@@ -11,7 +11,8 @@ cp ./zig-out/bin/telexify ~/nlp
 ~/nlp/telexify data/news_titles.txt data/tknz.txt # warm up
 # ~/nlp/telexify ../results/news_titles.aa ../results/news_titles.aa.xyz
 
-# Cross-platform build
-# ./clean.sh && zig build -Dtarget=x86_64-windows
+# Windows cross-platform build
 # Wine Is Not an Emulator: translates Windows API calls into POSIX calls on-the-fly
 # brew install wine-stable # wine64 file.exe
+# ./clean.sh && zig build -Dtarget=x86_64-windows -Drelease-safe=true
+# wine64 zig-out/bin/telexify.exe data/news_titles.txt data/tknz.txt
