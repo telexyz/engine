06337eadd09d78718a558ea1d27d2f16983e86a6
Tien Dung <dungtn@gmail.com>
2021-08-03 10:21:45 +0700

    prepare dictionary to do syllables2words

--- run.sh
+++ run.sh
@@ -5,13 +5,15 @@ rm data/1*.txt
 rm data/2*.txt
 rm data/*tknz.txt
 
-~/zig build -Drelease-fast=true
-# ~/zig build
-cp ./zig-out/bin/telexify ~/bin
+# ~/zig build -Drelease-fast=true
+# cp ./zig-out/bin/telexify ~/bin
 # cp ./zig-out/bin/telexify ~/repos/results/bin
-~/bin/telexify data/news_titles.txt data/tknz.txt parts ngram # warm up
-# ~/bin/telexify ../results/news_titles.aa ../results/news_titles.aa.xyz
+# ~/bin/telexify data/news_titles.txt data/tknz.txt parts ngram # warm up
 
+~/zig build
+~/bin/telexify dict/VnVocab.txt dict/VnVocab.xyz dense ngram # warm up
+
+# - - - - - - - - - - - - - - 
 # Windows cross-platform build
 # Wine Is Not an Emulator: translates Windows API calls into POSIX calls on-the-fly
 # brew install wine-stable # wine64 file.exe
