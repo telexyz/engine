b2887758c9b294539a6ef503ef5cbf6b1f94f0bc
Tien Dung <dungtn@gmail.com>
2021-08-09 14:10:08 +0700

    add wordlist.xyz that have 2-4 syll words only

--- dict/vocab.rb
+++ dict/vocab.rb
@@ -1,11 +1,16 @@
 # Use /( .+?){3}\n/ to match 3-syll word
 
-words = File.open("wordlist.txt").read.split("\n").uniq
- 
-words_sylls = words.map{ |w| w.split(" ") }
+words = File.open("wordlist.xyz").read.split("\n").uniq
 
-words_sylls.sort_by! { |x|
-	x.size
+selected = [] 
+words.each{ |w| 
+	a = w.split(" ") 
+	selected << a.join(" ").gsub("|","_") if a.size >= 2 and a.size <= 4
 }
 
-puts words_sylls.map{ |x| x.join(" ") }
+selected.sort!
+puts selected.map{ |x| x.gsub("_","|") }
+
+# words_sylls = words.map{ |w| w.split(" ") }
+# words_sylls.sort_by! { |x| x.size }
+# puts words_sylls.map{ |x| x.join(" ") }
