a2f1c7eb41831498c52b74465c6312e411e7e0ca
Tien Dung <dungtn@gmail.com>
2021-08-10 18:17:00 +0700

    correct .xyz

--- dict/vocab.rb
+++ dict/vocab.rb
@@ -1,16 +1,17 @@
 # Use /( .+?){3}\n/ to match 3-syll word
-
 words = File.open("wordlist.xyz").read.split("\n").uniq
 
 selected = [] 
 words.each{ |w| 
-	a = w.split(" ") 
-	selected << a.join(" ").gsub("|","_") if a.size >= 2 and a.size <= 4
+	a = w.split(" ")
+	n = a.size
+	next if w =~ /[^\sa-z\|]/
+	next if n < 2 or n > 4
+	next if w.count("|") != n
+	selected << a.join(" ").gsub("|","`") 
 }
-
-selected.sort!
-puts selected.map{ |x| x.gsub("_","|") }
-
+puts selected.sort.map{ |x| x.gsub("`","|") }.uniq
+ 
 # words_sylls = words.map{ |w| w.split(" ") }
 # words_sylls.sort_by! { |x| x.size }
 # puts words_sylls.map{ |x| x.join(" ") }
