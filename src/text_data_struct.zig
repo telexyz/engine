371aeef926929221a632b35e39d817d23b101356
Tien Dung <dungtn@gmail.com>
2021-08-17 12:39:05 +0700

    revert invisible char mode to write token attrs

--- src/text_data_struct.zig
+++ src/text_data_struct.zig
@@ -158,15 +158,37 @@ pub const Text = struct {
         pub inline fn haveMarkTone(self: TypeInfo) bool {
             return self.category == .syllmark or self.category == .alphmark;
         }
+
+        pub inline fn toByte(self: TokenAttributes) u8 {
+            const byte = @bitCast(u8, self);
+            if (byte < 12) return byte + 1;
+            return byte;
+        }
+
+        pub inline fn newFromByte(byte: u8) TokenAttributes {
+            if (byte < 12) byte -= 1;
+            return @bitCast(TokenAttributes, byte);
+        }
     };
 
-    pub const TokenCategory = enum(u3) {
-        to_parse_syllable,
-        nonalpha,
-        alphmark,
-        alph0m0t,
-        syllmark,
-        syll0m0t,
+    pub const TokenCategory = enum(u6) {
+        // Dùng được 27 invisible ascii chars, 1-8,11, 12,15-31
+        // 3 main token categoried, used to write to disk as token's attrs
+
+        // Final states
+        nonalpha = 0, //  + 2-bits  => 00,01,02,03 + 1 => \x01\x02\x03\x04
+        // Avoid slot 1 if possible since it don't show as space in klogg app
+        //         1, //  + 2-bits  => 04,05,06,07 + 1 => \x05\x06\x07\x08
+        //         2, //  + 0x11    =>       10    + 1 => \x0b
+        //         3, //  + 0x00,11 => 12       15     => \x0c\x0f
+        alphmark = 4, //  + 2-bits  => 16,17,18,19     => \x10\x11\x12\x13
+        alph0m0t = 5, //  + 2-bits  => 20,21,22,23     => \x14\x15\x16\x17
+        syllmark = 6, //  + 2-bits  => 24,25,26,27     => \x18\x19\x1a\x1b
+        syll0m0t = 7, //  + 2-bits  => 28,29,30,31     => \x1c\x1d\x1e\x1f
+
+        // Supplement category ids 8-63
+        // used as an intialized / transit states
+        to_parse_syllable = 8,
     };
 
     pub const TokenSurroundedBySpaces = enum(u2) {
