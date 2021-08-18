95fdc499d081a3fba52d3c70a0f817de5d6d9280
Tien Dung <dungtn@gmail.com>
2021-08-18 09:07:16 +0700

    update

--- run.sh
+++ run.sh
@@ -12,17 +12,14 @@
 # ./zig-out/bin/telexify data/sample_input.txt data/sample_output.txt dense ngram
 # ./zig-out/bin/telexify data/sample_input.txt data/sample_output.txt spare
 # ./zig-out/bin/telexify data/sample_input.txt data/sample_output.txt parts
-# ./zig-out/bin/telexify data/sample_input.txt data/sample_output.txt dense ngram
-./zig-out/bin/telexify ../data/fb_comments.txt ../data/fb_comments.dense.xyz dense
-# ./zig-out/bin/telexify ../data/vietai_sat.txt ../data/vietai_sat.dense.xyz dense
+./zig-out/bin/telexify data/sample_input.txt data/sample_output.txt dense ngram
 
-# ~/bin/telexify ../data/combined.txt ../data/combined.xyz dense
-# ~/bin/telexify ../data/fb_comments.txt ../data/fb_comments.dense.xyz dense ngram
-# ~/bin/telexify ../data/vietai_sat.txt ../data/vietai_sat.dense.xyz dense ngram
-# ~/bin/telexify ../data/vi_wiki_all.txt ../data/vi_wiki_all.spare.xyz spare ngram
-# ~/bin/telexify ../data/opensub.vi ../data/opensub.xyz dense ngram
+./zig-out/bin/telexify ../data/combined.txt ../data/combined.xyz dense
+# ./zig-out/bin/telexify ../data/fb_comments.txt ../data/fb_comments.dense.xyz dense ngram
+# ./zig-out/bin/telexify ../data/vietai_sat.txt ../data/vietai_sat.dense.xyz dense ngram
+# ./zig-out/bin/telexify ../data/vi_wiki_all.txt ../data/vi_wiki_all.spare.xyz spare ngram
+# ./zig-out/bin/telexify ../data/opensub.vi ../data/opensub.xyz dense ngram
 
-# ~/zig run src/play_with_chars.zig
 # - - - - - - - - - - - - - - 
 # Windows cross-platform build
 # Wine Is Not an Emulator: translates Windows API calls into POSIX calls on-the-fly
