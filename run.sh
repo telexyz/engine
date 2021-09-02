014eefa1a4fa538c31e4ae51bd788c5d3dbb91fe
Tien Dung <dungtn@gmail.com>
2021-09-02 10:18:41 +0700

    refine

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# rm data/0?-*.txt data/1?-*.txt data/3?-*.txt data/temp*
+rm data/temp* # data/0?-*.txt data/1?-*.txt data/3?-*.txt
 # rm -rf ***/zig-cache ***/zig-out
 
 ~/zig build -Drelease-safe=true && cp ./zig-out/bin/telexify ~/bin
-# ~/zig build -Drelease-fast=true && cp ./zig-out/bin/telexify ~/bin
 # ~/zig build
 
 # ./zig-out/bin/telexify data/sample_input.txt data/sample_output.txt spare
@@ -12,18 +11,11 @@
 # ./zig-out/bin/telexify data/sample_input.txt data/sample_output.txt dense
 # ~/zig run src/count_n_gram.zig -- data/sample_output.txt.cdx
 
-./zig-out/bin/telexify ../data/combined.txt ../data/combined.xyz dense ngram
-# ~/zig run src/count_n_gram.zig -O ReleaseSafe -- ../data/combined.xyz.cdx
+# ./zig-out/bin/telexify ../data/combined.txt ../data/combined.xyz dense ngram
+~/zig run src/count_n_gram.zig -O ReleaseSafe -- ../data/combined.xyz.cdx
 
 # ./zig-out/bin/telexify ../data/news_titles.txt ../data/news_titles.xyz dense
 # ./zig-out/bin/telexify ../data/fb_comments.txt ../data/fb_comments.dense.xyz dense ngram
 # ./zig-out/bin/telexify ../data/vietai_sat.txt ../data/vietai_sat.dense.xyz dense
 # ./zig-out/bin/telexify ../data/vi_wiki_all.txt ../data/vi_wiki_all.spare.xyz spare
 # ./zig-out/bin/telexify ../data/opensub.vi ../data/opensub.xyz dense
-
-# - - - - - - - - - - - - - - 
-# Windows cross-platform build
-# Wine Is Not an Emulator: translates Windows API calls into POSIX calls on-the-fly
-# brew install wine-stable # wine64 file.exe
-# ~/zig build -Dtarget=x86_64-windows -Drelease-safe=true
-# wine64 zig-out/bin/telexify.exe data/sample_input.txt data/sample_output.txt
