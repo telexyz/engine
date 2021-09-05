58aec32bbd6c642c51ccd2fa141a2fd81e16b468
Tien Dung <dungtn@gmail.com>
2021-09-05 15:52:45 +0700

    Absoluted Discount Smoothing + Stupid Backoff for the sake of Simplicity

--- src/counting/language_model.zig
+++ src/counting/language_model.zig
@@ -6,8 +6,10 @@
 //   See `docs/n-gram_smoothing.md` (Kneser-Ney is the best but require more
 //   computation and the bigger the corpus is the nearer they are converged)
 
+// Total Mem 501 MB
+//
 // * 268 MB `BinaryFuseFilter`: 16-bits per key grams with count = 1
 // *  30 MB `BinaryFuseFilter`: 16-bits per key grams with count = 2
-
+//
 // * 131 MB `HashCount234`: 7-bytes key represent + 2-byte count for 4,5,6-grams
 // *  72 MB `HashCount234`: 6-bytes key represent + 3-byte count for 1,2,3-grams
