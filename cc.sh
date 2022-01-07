2986a641902b6d1686ca9aafab1e029fe475a644
Tien Dung <dungtn@gmail.com>
2022-01-07 16:06:34 +0700

    refine

--- cc.sh
+++ cc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 scc --exclude-dir src/lib --exclude-dir .save -M \.txt --no-cocomo
-rm -rf zig-cache zig-out && ls
-time ~/zig build test
+# rm -rf zig-cache zig-out && ls
+time zig build test
