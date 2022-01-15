19442c41fa57d7f399b2714936d699e80fb266e4
Tien Dung <dungtn@gmail.com>
2022-01-15 17:44:58 +0700

    update

--- cc.sh
+++ cc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 scc --exclude-dir src/lib --exclude-dir .save -M \.txt --no-cocomo
-# rm -rf zig-cache zig-out
+rm -rf zig-cache zig-out
 lsd -F --group-dirs=first src
 time /Users/t/repos/zig/build/zig build test
