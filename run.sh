48d6b3cfe81ba5acb6b6a322672e90c4423c58f6
Tien Dung <dungtn@gmail.com>
2022-01-21 16:15:08 +0700

    try to use zig stage2

--- run.sh
+++ run.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-# rm -rf ***/zig-cache ***/zig-out
-# zig build -Drelease-fast=true # && cp ./zig-out/bin/telexify ~/bin
+rm -rf ***/zig-cache ***/zig-out
+~/zz build -Drelease-fast=true # && cp ./zig-out/bin/telexify ~/bin
 
 # ./zig-out/bin/telexify data/sample_input.txt data/sample_output.txt spare
 # ./zig-out/bin/telexify data/sample_input.txt data/sample_output.txt parts
 # ./zig-out/bin/telexify data/sample_input.txt data/sample_output.txt dense
 # zig run src/count_n_gram.zig -- data/sample_output.txt.cdx
 
-# ./zig-out/bin/telexify ../data/combined.txt ../data/combined.xyz dense
-zig run src/count_n_gram.zig -O ReleaseFast -- ../data/combined.xyz.cdx
-
+./zig-out/bin/telexify ../data/combined.txt ../data/combined.xyz dense
+# ~/zz run src/count_n_gram.zig -O ReleaseFast -- ../data/combined.xyz.cdx
 
 # rm data/??-*
-# ~/zig build
+# ~/z build
 # ./zig-out/bin/telexify ../phaps/data/all.txt data/phaps.utf8 utf8
-# ~/zig run src/count_n_gram.zig -- data/phaps.utf8.cdx
+# ~/z run src/count_n_gram.zig -- data/phaps.utf8.cdx
