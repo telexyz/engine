3c75daeeb19e844fd39ff7ac54790b98631d058b
Tien Dung <dungtn@gmail.com>
2022-01-29 00:07:55 +0700

    refine

--- src/embedding/build.sh
+++ src/embedding/build.sh
@@ -3,4 +3,4 @@
 # cc word2vec.c -o word2vec -lm -pthread -O3 -march=native -Wall -funroll-loops -Wno-unused-result
 
 rm word2vec
-~/z build-exe -lc word2vec.c -O ReleaseFast
+zig build-exe -lc word2vec.c # -O ReleaseFast
--- src/embedding/run.sh
+++ src/embedding/run.sh
@@ -4,31 +4,25 @@
 # conda config --env --add channels conda-forge
 # conda install numpy scipy
 
-# Edited from https://raw.githubusercontent.com/zhezhaoa/ngram2vec/master/ngram_example.sh
-memory_size=4
-cpus_num=2
-corpus=Phaps.xyz
-output_path=data
+mkdir -p data
 
-mkdir -p ${output_path}
+python ngram2vec/corpus2vocab.py --corpus_file Phaps.xyz --vocab_file data/vocab --memory_size 4 --feature ngram --order 2
 
-python ngram2vec/corpus2vocab.py --corpus_file ${corpus} --vocab_file ${output_path}/vocab --memory_size ${memory_size} --feature ngram --order 2
-
-python ngram2vec/corpus2pairs.py --corpus_file ${corpus} --pairs_file ${output_path}/pairs --vocab_file ${output_path}/vocab --processes_num ${cpus_num} --cooccur ngram_ngram --input_order 1 --output_order 2
+python ngram2vec/corpus2pairs.py --corpus_file Phaps.xyz --pairs_file data/pairs --vocab_file data/vocab --processes_num 2 --cooccur ngram_ngram --input_order 1 --output_order 2
 
 # Concatenate pair files. 
-if [ -f "${output_path}/pairs" ]; then
-	rm ${output_path}/pairs
+if [ -f "data/pairs" ]; then
+	rm data/pairs
 fi
-for i in $(seq 0 $((${cpus_num}-1)))
+for i in $(seq 0 1)
 do
-	cat ${output_path}/pairs_${i} >> ${output_path}/pairs
-	rm ${output_path}/pairs_${i}
+	cat data/pairs_${i} >> data/pairs
+	rm data/pairs_${i}
 done
 
 # Generate input vocabulary and output vocabulary, which are used as vocabulary files for all models
-python ngram2vec/pairs2vocab.py --pairs_file ${output_path}/pairs --input_vocab_file ${output_path}/vocab.input --output_vocab_file ${output_path}/vocab.output
+python ngram2vec/pairs2vocab.py --pairs_file data/pairs --input_vocab_file data/vocab.input --output_vocab_file data/vocab.output
 
 # SGNS, learn representation upon pairs.
 # We add a python interface upon C code.
-python ngram2vec/pairs2sgns.py --pairs_file ${output_path}/pairs --input_vocab_file ${output_path}/vocab.input --output_vocab_file ${output_path}/vocab.output --input_vector_file ${output_path}/sgns.input --output_vector_file ${output_path}/sgns.output --threads_num ${cpus_num} --size 300
+./word2vec --pairs_file data/pairs --input_vocab_file data/vocab.input --output_vocab_file data/vocab.output --input_vector_file data/sgns.input --output_vector_file data/sgns.output --threads_num 2 --size 300
